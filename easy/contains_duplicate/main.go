c5e736fe2ef92127f45682a11df479b7abd1a99c
Alexei <dark_sao@mail.ru>
2026-04-27 09:59:16 +0300

    second task

--- easy/contains_duplicate/main.go
+++ easy/contains_duplicate/main.go
@@ -1,12 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	fmt.Println("TODO: solve")
+	fmt.Println(containsDuplicate([]int{1, 2, 3, 1}))                   // true
+	fmt.Println(containsDuplicate([]int{1, 2, 3, 4}))                   // false
+	fmt.Println(containsDuplicate([]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2})) // true
 }
 
 func containsDuplicate(nums []int) bool {
-	_ = nums
+	if len(nums) < 2 {
+		return false
+	}
+
+	sort.Ints(nums)
+
+	for i := 0; i < len(nums); i++ {
+		if i == 0 {
+			continue
+		}
+
+		if nums[i] == nums[i-1] {
+			return true
+		}
+	}
+
 	return false
 }
