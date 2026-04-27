c6a264b43b8c800bd4a0ce872d9a9eb549f0de01
Alexei <dark_sao@mail.ru>
2026-04-27 09:26:18 +0300

    feat(easy): add current two_sum solution

--- easy/two_sum/main.go
+++ easy/two_sum/main.go
@@ -3,10 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{2, 7, 11, 15}
-	fmt.Println(twoSum(arr, 9))
+	arr := []int{3, 3}
+	fmt.Println(twoSum(arr, 6))
 }
 
 func twoSum(nums []int, target int) []int {
-	return []int{0, 1}
+	values := make(map[int]int)
+
+	for i := range nums {
+		if i == 0 {
+			values[nums[i]] = i
+			continue
+		}
+
+		diff := target - nums[i]
+		if idx, ok := values[diff]; ok {
+			return []int{idx, i}
+		}
+
+		values[nums[i]] = i
+	}
+
+	return nums
 }
